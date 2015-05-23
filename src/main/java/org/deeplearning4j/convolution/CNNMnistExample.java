371c6de2951282147d2758504102afb22cc202d4
nyghtowl <warrick.melanie@gmail.com>
2015-05-23 08:38:11 -0700

    Working on applying parameters based on a mix of readings from Theano to other sources on ideal parameters.

--- src/main/java/org/deeplearning4j/convolution/CNNMnistExample.java
+++ src/main/java/org/deeplearning4j/convolution/CNNMnistExample.java
@@ -1,5 +1,7 @@
 package org.deeplearning4j.convolution;
 
+import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
+import org.deeplearning4j.datasets.iterator.SamplingDataSetIterator;
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
@@ -19,6 +21,7 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,34 +38,39 @@ public class CNNMnistExample {
 
         final int numRows = 28;
         final int numColumns = 28;
-        int batchSize = 10; // TODO return to 1802
+        int batchSize = 50;
+        int numSamples = 500;
+        double numTrainSamples = numSamples * 0.8;
 
         log.info("Load data....");
-        DataSetIterator mnist = new MnistDataSetIterator(100,100); // TODO change back to 100,1000 (batch, numExamples) - there are 60k avail
-        DataSet all = mnist.next();
-        all.normalizeZeroMeanZeroUnitVariance();
+        DataSetIterator mnist = new MnistDataSetIterator(numSamples,numSamples); // TODO there are 60k avail
+        DataSet allMnist = mnist.next();
+        allMnist.normalizeZeroMeanZeroUnitVariance();
 
         log.info("Split data....");
-        SplitTestAndTrain trainTest = all.splitTestAndTrain(90); // train set that is the result - should flip // TODO put back to 80% of data
+        SplitTestAndTrain trainTest = allMnist.splitTestAndTrain(((int) numTrainSamples)); // train set that is the result - should flip // TODO put back to 80% of data
         DataSet trainInput = trainTest.getTrain(); // get feature matrix and labels for training
         INDArray testInput = trainTest.getTest().getFeatureMatrix();
         INDArray testLabels = trainTest.getTest().getLabels();
+
+        DataSetIterator trainDataSetIterator = new SamplingDataSetIterator(trainInput, batchSize, (int) numTrainSamples);
 
         log.info("Build model....");
         // TODO iterations back to 10
         // TODO try the if then without the input and preprocess for all layers
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .nIn(numRows * numColumns).nOut(10).batchSize(batchSize)
-                .iterations(4).weightInit(WeightInit.VI)
-                .activationFunction("sigmoid").filterSize(7, 1, numRows, numColumns)
-                .optimizationAlgo(OptimizationAlgorithm.LBFGS).constrainGradientToUnitNorm(true)
-                .list(3).hiddenLayerSizes(new int[]{50})
+                .iterations(100).weightInit(WeightInit.ZERO)
+                .activationFunction("sigmoid").filterSize(30, 1, numRows, numColumns)
+                .lossFunction(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD).learningRate(0.13)
+                .optimizationAlgo(OptimizationAlgorithm.GRADIENT_DESCENT).constrainGradientToUnitNorm(true)
+                .list(3).hiddenLayerSizes(new int[]{72})
                 .inputPreProcessor(0, new ConvolutionInputPreProcessor(numRows, numColumns)).preProcessor(1, new ConvolutionPostProcessor())
                 .override(0, new ConfOverride() {
                     public void overrideLayer(int i, NeuralNetConfiguration.Builder builder) {
                         builder.layer(new ConvolutionLayer());
                         builder.convolutionType(ConvolutionLayer.ConvolutionType.MAX);
-                        builder.featureMapSize(9, 9);
+                        builder.featureMapSize(11, 11);
                     }
                 }).override(1, new ConfOverride() {
                     public void overrideLayer(int i, NeuralNetConfiguration.Builder builder) {
@@ -75,12 +83,16 @@ public class CNNMnistExample {
 
         log.info("Train model....");
         network.setListeners(Arrays.<IterationListener>asList(new ScoreIterationListener(1)));
-        network.fit(trainInput);
+
+        while(trainDataSetIterator.hasNext()) {
+            DataSet trainBatchData = trainDataSetIterator.next();
+            network.fit(trainBatchData);
+        }
 
         log.info("Evaluate model....");
         Evaluation eval = new Evaluation();
         INDArray output = network.output(testInput);
-        eval.eval(testLabels,output);
+        eval.eval(testLabels, output);
         log.info(eval.stats());
         log.info("****************Example finished********************");
 
